993262c8fdc8430eee41884190970c78da665de6
Royner Farías <fariasroyner@gmail.com>
2026-09-12 15:32:28 -0400

    cambio

--- backend/config/firebase.js
+++
@@ -1,7 +0,0 @@
-// Configuración de Firebase Admin SDK
-// Debes descargar tu archivo de credenciales de Firebase Console
-// y guardarlo como firebase-service-account.json
-
-module.exports = {
-  // Tu configuración de Firebase
-};
