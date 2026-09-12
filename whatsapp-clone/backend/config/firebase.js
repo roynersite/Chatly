dc054dd39c72048a94559542948cf1f20f0497c9
Royner Farías <fariasroyner@gmail.com>
2026-09-12 14:08:18 -0400

    cambios

---
+++ whatsapp-clone/backend/config/firebase.js
@@ -0,0 +1,7 @@
+// Configuración de Firebase Admin SDK
+// Debes descargar tu archivo de credenciales de Firebase Console
+// y guardarlo como firebase-service-account.json
+
+module.exports = {
+  // Tu configuración de Firebase
+};
